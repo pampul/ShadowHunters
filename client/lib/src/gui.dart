bd2f3f1b6a30f0bb63409db642869b0229a7b89b
Florian Mithieux <florian.mithieux@gmail.com>
2014-06-13 17:09:26 +0200

    Add AngularDart

--- client/lib/src/gui.dart
+++ client/lib/src/gui.dart
@@ -2,3 +2,4 @@ part of client_library;
 
 ButtonElement submitMsg = querySelector("#submitMsg");
 InputElement inputElement = querySelector('#msg');
+ImageElement imageElement = querySelector('#google-sign-in');
--- client/web/app.dart
+++ client/web/app.dart
@@ -1,7 +1,16 @@
 import 'dart:html';
 import 'package:ShadowHunters_Client/client_library.dart';
+import 'package:google_oauth2_client/google_oauth2_browser.dart';
 
 void main() {
+
+  var signIn = new ButtonElement();
+  signIn.text = "Sign in with Google";
+  signIn.onClick.listen((e) {
+    auth.login();
+  });
+
+  document.body.children.add(signIn);
 
   // Instantiating a new Game() class
   Game game = new Game();
@@ -10,9 +19,16 @@ void main() {
   Connection conn = new Connection("ws://" + window.location.host + "/ws", game);
 
   submitMsg.onClick.listen((e) {
-
     conn.send({"cmd":"getMessage", "arg": inputElement.value});
-
   });
 
+}
+
+final auth = new GoogleOAuth2(
+    "227440942862-g6auj4rmi0kbbn6arpina7ofjukn4qn4.apps.googleusercontent.com",
+    ["openid", "email"],
+    tokenLoaded:oauthReady);
+
+void oauthReady(Token token) {
+  print(token);
 }
